55af2850937e7250cb7f61f96cf7c7aec47db640
Ruslan <54503675+RiveraRuslan@users.noreply.github.com>
2023-03-19 21:53:54 +0600

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+//Language
+$('#header__language').click(function () {
+  if ($(this).html() === 'RU') {
+    $(this).html('EN');
+  } else {
+    $(this).html('RU');
+  }
+  return false;
+});
+
+//Burger
+const menuBtn = $('.menu__btn');
+const menu = $('.menu');
+const body = $('.body');
+
+menuBtn.on('click', function () {
+  menuBtn.toggleClass('menu__btn--active');
+  menu.toggleClass('menu__list--active');
+  body.toggleClass('lock');
+});
+
+menu.on('click', '.menu__list-link', function () {
+  menuBtn.removeClass('menu__btn--active');
+  menu.removeClass('menu__list--active');
+  body.toggleClass('lock');
+});
+
+//Accordion
+const questionInner = $('.accordion__item-inner');
+const questionItem = $('.accordion__item');
+
+questionInner.on('click', function () {
+  questionItem.removeClass('questions__item--active');
+  $(this).parent().addClass('questions__item--active');
+});
+
+//Animate
+var animations = [
+  { container: document.getElementById('animation__flower'), path: '/img/json/Flower.json' },
+  { container: document.getElementById('animation__kitten'), path: '/img/json/Kitten.json' },
+];
+
+animations.forEach(function (animation) {
+  bodymovin.loadAnimation({
+    container: animation.container,
+    path: animation.path,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    name: 'Demo Animation',
+  });
+});
